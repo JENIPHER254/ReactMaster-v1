1c7cc2c372df114ffe65fdbdbb44d28ca146af1a
JENIPHER <jeniperkuki@gmail.com>
2024-03-30 15:01:16 +0300

    declaration/binding of all functions

--- music-app/src/components/App/App.js
+++ music-app/src/components/App/App.js
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import playlist from '../playlist/playlist';
+import searchbar from "../searchbar/searchbar";
+import searchresults from "../searchresults/searchresults";
+import spotify from "../utils/spotify";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    
+    this.state={
+      searchresults :[],
+      playlistname: "playlist",
+      playlisttracks: [],
+  
+    }
+    //databinding
+    this.search = this.search.bind(this);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updataPlaylistName = this.updataPlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.removeTrackSearch = this.removeTrackSearch.bind(this);
+    this.doThese = this.doThese.bind(this);
+  }
+  
+}
 
 function App() {
   return (
