7d074285b4e67f74c883300ecb4aea60529da48a
JENIPHER254 <jeniperkuki@gmail.com>
2025-04-10 18:55:23 +0300

    feat: finished responsive header section

--- react_learner/src/components/Header.jsx
+++ react_learner/src/components/Header.jsx
@@ -3,23 +3,13 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./../css/App.css";
-("");
 
 function HeaderSection() {
   return (
     <div className="header">
       <div className="header-cover ">
-        <style type="text/css">
-          {`
-    .fontsize1{
-      font-size: 0.8rem;
-    }
-  
-    `}
-        </style>
         <div className="justify-content-center align-items-center d-flex">
           <Navbar expand="lg" className="bg-body-transparent  nav-width ">
             <Container fluid>
@@ -54,13 +44,28 @@ function HeaderSection() {
                 </Nav.Link>
 
                 <Form className="d-flex">
-                  <Button variant="outline-light" className="px-3">
+                  <Button
+                    variant="outline-light"
+                    className=" rounded-0 border-2 fw-bold contact-buttons"
+                  >
                     Contact Us
                   </Button>
                 </Form>
               </Navbar.Collapse>
             </Container>
           </Navbar>
+        </div>
+        <div className="row main-row-header">
+          <div className="col-sm-6">
+            <br />
+            <div className=" text-light h6">WELCOME TO DATAPRANA</div> <br />
+            <div className="text-light h1 ">CONNECTING DATA AND ENERGY</div>
+            <br />
+            <div className="p text-light">
+              We build and Operate Data Centers for High Performance <br />{" "}
+              Quality powering the Decentralized Economy <br /> of the Future
+            </div>
+          </div>
         </div>
       </div>
     </div>
